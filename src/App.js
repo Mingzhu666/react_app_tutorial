ec061b672e38c23acde96b1de4a898821ade1352
万明珠 <mingzhuwan@gmail.com>
2021-09-10 17:44:36 +1000

    update: fist commit to react env project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ProjectSearchList } from "screens/project-list"
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectSearchList />
+    </div>
+  );
+}
+
+export default App;
